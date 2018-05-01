06bcb207b956c926833947ce55b0720b0a2f067d
mmanderson009 <mercedesanderson@nevada.unr.edu>
2018-04-30 18:11:51 -0700

    Basic Continual Development.

--- mapper.py
+++ mapper.py
@@ -11,14 +11,15 @@ OAUTH_TOKEN_SECRET = 'ERS7aq8DYOkoh0JsG5JNvE1ulAo3msTtuq2UQ8sc4DzXR'
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 location = None
-
-searchLocation = input("Where would you like to search?")
+chosenUser = None
+searchLocation = input("Where would you like to search? | Ex:\"san franscisco\" | ")
+print searchLocation
 
 fileOut = open('userLocations.txt','w')
 
 #while location == None:
 try:
-    search_results = twitter.search(q = searchLocation, count = 50)
+    search_results = twitter.search(q = searchLocation, count = 150)
 
 except TwythonError as e:
     print e
@@ -33,6 +34,10 @@ for tweet in search_results['statuses']:
 		fileOut.write(tweet['user']['screen_name'].encode('utf-8'))
 		fileOut.write(" : " + ' '.join(str(e) for e in location) + '\n')
 
+		chosenUser = tweet['user']['screen_name'].encode('utf-8')
+
+
+print "The chosenUser is " , chosenUser , '\n'
 fileOut.close()
 
 '''
@@ -62,6 +67,5 @@ urlComplete = urlStart + urlOrigin + urlWaypoint + urlDestination + urlMode
 print urlComplete
 
 webbrowser.open(urlComplete, new=1, autoraise=True)
-
 
 '''
